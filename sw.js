1469e3a47ace3a1b0c03d5bd38d878c896ebfeb3
Labeebalkalai <102527419+Labeebalkalai@users.noreply.github.com>
2026-05-11 07:20:09 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'amwaj-v1';
+const ASSETS = [
+    './',
+    './index.html',
+    './css/style.css',
+    './js/main.js',
+    './logo.jpeg',
+    './dev_labib_real.jpg'
+];
+
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS);
+        })
+    );
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then((res) => {
+            return res || fetch(e.request);
+        })
+    );
+});
